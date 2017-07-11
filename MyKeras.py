fd563d7e8ac1964e2443ba9b432917127b8ce62a
Buddhika <buddhika@umiami.edu>
2017-07-11 00:25:30 -0400

    Initial model for tweet counts

--- MyKeras.py
+++ MyKeras.py
@@ -6,20 +6,47 @@ from keras.layers.core import Activation, Dense
 from keras.layers.recurrent import LSTM
 from keras.optimizers import SGD
 
-def GenTrainData(data_len,n_samples):
+#def GenTrainData(data_len,n_samples):
+Seq_Size = 10
+def GenTrainData():
 
-	dc = .5
-	X = np.zeros((n_samples,1,3*data_len/4))
-	t = np.linspace(0,20*np.pi,data_len);
-	Y = np.zeros((n_samples,data_len/4))
-	for i in range(0,n_samples):
-		amp = np.random.rand(1)*2
-		#print amp
-		full_Vec = dc +np.sin(amp*t)/4
-		X[i,0,:] = full_Vec[0:data_len*3/4]
-		Y[i,:] = full_Vec[data_len*3/4:data_len]
-	
-	return X,Y
+	#dc = .5
+	#X = np.zeros((n_samples,1,3*data_len/4))
+	#t = np.linspace(0,20*np.pi,data_len);
+	#Y = np.zeros((n_samples,data_len/4))
+	#for i in range(0,n_samples):
+	#	amp = np.random.rand(1)*2
+	#	#print amp
+	#	full_Vec = dc +np.sin(amp*t)/4
+	#	X[i,0,:] = full_Vec[0:data_len*3/4]
+	#	Y[i,:] = full_Vec[data_len*3/4:data_len]
+	#
+	#return X,Y
+	alldata = np.genfromtxt('xystructured100.txt')
+	n_data = alldata.shape[0]/2
+	n_train = int(n_data*.9*2)
+	print n_train
+	n_test = n_data-n_train
+	Xtemp = alldata[0:n_train,0:Seq_Size]
+	Y = alldata[range(0,n_train,2),Seq_Size]
+	Xtest = alldata[n_train:n_data*2,0:Seq_Size]
+	Ytest = alldata[range(n_train,n_data*2,2),Seq_Size]
+	X = np.zeros((n_train/2,2,Seq_Size))
+	#Y = np.zeros((n_train/2,1,1))
+	for i in range(0,n_train/2):
+		print i
+		X[i,0,:] = Xtemp[2*i,:]	
+		X[i,1,:] = Xtemp[2*i+1,:]	
+	#print 'Train input \n',X
+	print 'Train input size \n',X.shape
+	#print 'Train output \n',Y
+	print 'Train output size \n',Y.shape
+	#print 'Test input \n',Xtest
+	#print 'Test output \n',Ytest
+	return X,Y,Xtest,Ytest
+
+
+
 
 def GenTestData(data_len):
 
@@ -35,33 +62,34 @@ def GenTestData(data_len):
 
 	return y,X
 
+#GenTrainData()
 	
-data_len = 1024
-n_samples = 800 # Number of train samples
-
+#data_len = 1024
+#n_samples = 800 # Number of train samples
+#
 model = Sequential()
-model.add(LSTM(1,input_shape=(1,data_len*3/4)))
-#model.add(LSTM(1, input_shape=(1,), activation='tanh',
-#					recurrent_activation='hard_sigmoid'))
-model.add(Dense(data_len/4, activation='sigmoid'))
-
-t = np.linspace(0,2*np.pi,data_len);
-amp = .4
-noise_var = .01
-
-X_train,Y_train = GenTrainData(data_len,n_samples)
-
+model.add(LSTM(1,input_shape=(2,Seq_Size)))
+##model.add(LSTM(1, input_shape=(1,), activation='tanh',
+##					recurrent_activation='hard_sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
+#
+#t = np.linspace(0,2*np.pi,data_len);
+#amp = .4
+#noise_var = .01
+#
+X,Y,Xtest,Ytest = GenTrainData()
+#
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='mean_squared_error', optimizer=sgd)
-
-history = model.fit(X_train, Y_train, epochs=1000, batch_size=4, verbose=0)
-
-y,X_test = GenTestData(data_len)
-#X = dc+amp*np.sin(t-np.pi/2)/2
-Op =  model.predict(X_test)
-print 'True out ',y
-print 'Predicted out ',Op
-output_sin, = plt.plot(t[3*data_len/4:data_len],Op[0,:],label='op')
-input_sin, = plt.plot(t[3*data_len/4:data_len],y,label='true ')
-plt.legend(handles=[output_sin, input_sin])
-plt.show()
+#
+history = model.fit(X, Y, epochs=100, batch_size=10, verbose=0)
+#
+#y,X_test = GenTestData(data_len)
+##X = dc+amp*np.sin(t-np.pi/2)/2
+#Op =  model.predict(X_test)
+#print 'True out ',y
+#print 'Predicted out ',Op
+#output_sin, = plt.plot(t[3*data_len/4:data_len],Op[0,:],label='op')
+#input_sin, = plt.plot(t[3*data_len/4:data_len],y,label='true ')
+#plt.legend(handles=[output_sin, input_sin])
+#plt.show()
