269102ef32799689cf4288106798112ef30c9490
Buddhika <buddhika@umiami.edu>
2017-07-13 00:45:34 -0400

    Some normalizing

--- MyKeras.py
+++ MyKeras.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 from keras.models import Sequential
 from keras.layers.core import Activation, Dense
 from keras.layers.recurrent import LSTM
@@ -22,9 +23,11 @@ def GenTrainData():
 	#	Y[i,:] = full_Vec[data_len*3/4:data_len]
 	#
 	#return X,Y
-	alldata = np.genfromtxt('xystructured.txt')
+	train_perc=.5
+	alldata = np.genfromtxt('xystructured100.txt')
+	#alldata = alldata - alldata.mean()
 	n_data = alldata.shape[0]/2
-	n_train = int(n_data*.9)*2
+	n_train = int(n_data*train_perc)*2
 	print '# Training samples ',n_train/2
 	n_test = n_data*2-n_train
 	print '# Testing samples ',n_test/2
@@ -36,11 +39,22 @@ def GenTrainData():
 	Xtest = np.zeros((n_test/2,Seq_Size,2))
 	#Y = np.zeros((n_train/2,1,1))
 	for i in range(0,n_train/2):
-		X[i,:,0] = Xtemp[2*i,:]	
-		X[i,:,1] = Xtemp[2*i+1,:]	
+		#ct_sum = np.sum(Xtemp[2*i,:])
+		X[i,:,0] = Xtemp[2*i,:]
+		#ngh_sum = np.sum(Xtemp[2*i+1,:])
+		X[i,:,1] = Xtemp[2*i+1,:]
+	X[:,:,0] -= X[:,:,0].mean()
+	X[:,:,1] -= X[:,:,1].mean()
 	for i in range(0,n_test/2):
+		ct_sum = np.sum(Xtemp_test[2*i,:])
 		Xtest[i,:,0] = Xtemp_test[2*i,:]
+		ngh_sum = np.sum(Xtemp_test[2*i+1,:])
 		Xtest[i,:,1] = Xtemp_test[2*i+1,:]
+	Xtest[:,:,0] -= Xtest[:,:,0].mean()
+	Xtest[:,:,1] -= Xtest[:,:,1].mean()
+
+	Y -= Y.mean()
+	Ytest -= Ytest.mean()
 	#print 'Train input \n',X
 	print 'Train input size \n',X.shape
 	#print 'Train output \n',Y
@@ -72,11 +86,12 @@ def GenTestData(data_len):
 #n_samples = 800 # Number of train samples
 #
 model = Sequential()
-model.add(LSTM(1,input_shape=(Seq_Size,2),return_sequences=True))
+model.add(LSTM(1,input_shape=(Seq_Size,2),activation='tanh',return_sequences=False))
 ##model.add(LSTM(1, input_shape=(1,), activation='tanh',
 ##					recurrent_activation='hard_sigmoid'))
-model.add(LSTM(Seq_Size))
+#model.add(LSTM(Seq_Size))
 model.add(Dense(Seq_Size, activation='sigmoid'))
+model.add(Dense(Seq_Size, activation='tanh'))
 model.add(Dense(1, activation='linear'))
 #
 #t = np.linspace(0,2*np.pi,data_len);
@@ -85,17 +100,28 @@ model.add(Dense(1, activation='linear'))
 #
 X,Y,Xtest,Ytest = GenTrainData()
 #
-sgd = SGD(lr=0.01, decay=1e-5, momentum=0.9, nesterov=False)
+sgd = SGD(lr=0.0001, decay=1e-5, momentum=0.9, nesterov=False)
 model.compile(loss='mean_squared_error', optimizer=sgd)
 #model.compile(loss='mean_absolute_error', optimizer=sgd)
 #
-history = model.fit(X, Y, epochs=500, batch_size=10, verbose=1)
+#print 'Input \n',X
+t1 = time.time()
+history = model.fit(X, Y, epochs=1000, batch_size=100, verbose=1)
+t2 = time.time()
+print model.get_weights()[0]
+print model.get_weights()[1]
+print model.get_weights()[2]
+np.savetxt('loss.txt',history.history['loss'])
 #
 #y,X_test = GenTestData(data_len)
 ##X = dc+amp*np.sin(t-np.pi/2)/2
+#print 'test input \n',Xtest
 Op =  model.predict(Xtest)
+XX = np.zeros((1,Seq_Size,2))
+#print 'allzeros input ',model.predict(XX)
 print 'True out ',Ytest
 print 'Predicted out ',Op
+print 'Training time ',t2-t1
 #output_sin, = plt.plot(t[3*data_len/4:data_len],Op[0,:],label='op')
 #input_sin, = plt.plot(t[3*data_len/4:data_len],y,label='true ')
 #plt.legend(handles=[output_sin, input_sin])
